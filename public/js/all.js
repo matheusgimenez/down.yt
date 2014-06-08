52ab9254fbd0ab7e15d0f2fa09ec4dc6901aefe2
matheuspetroni <contato@matheusgimenez.com.br>
2014-06-08 18:28:26 -0300

    form working

--- public/js/all.js
+++ public/js/all.js
@@ -2,3 +2,19 @@
  * Created by matheus on 06/06/14.
  */
 
+$('#url').on('submit', function(e){
+    e.preventDefault();
+    var url = $('#url input').val();
+    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    if (match&&match[2].length==11){
+        console.log(match[2]);
+        location.href = 'http://down.yt:3000/select/' + match[2];
+    }else{
+        $('#alert-form').show();
+    }
+});
+$('.close').on('click', function(e) {
+    location.reload();
+});
+
